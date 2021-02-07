02d87e0525ea124ceb02cde798bdaa5076570c1d
Takeshi Iwana <iwatakeshi@gmail.com>
2021-02-07 12:19:13 -0700

    Fix storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,28 @@
+const path = require('path');
+
+// https://github.com/storybookjs/storybook/issues/13277
+const toPath = _path => path.join(process.cwd(), _path);
+
 module.exports = {
   stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
+  },
+  webpackFinal: async config => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': toPath('node_modules/@emotion/react'),
+          '@emotion/styled': toPath('node_modules/@emotion/styled'),
+          'emotion-theming': toPath('node_modules/@emotion/react'),
+          '@emotion/styled-base': toPath('node_modules/@emotion/styled'),
+        },
+      },
+    };
   },
 };
